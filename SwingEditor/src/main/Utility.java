646ac349a984ad1b5aba3b19ae27304908ebb2c5
karlin13 <skfk0135@gmail.com>
2017-06-06 12:12:58 +0900

    #2 gencode, save, save as, open 할 때 파일 확장자 검사함

--- SwingEditor/src/main/Utility.java
+++ SwingEditor/src/main/Utility.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 import java.util.List;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -43,13 +44,16 @@ public class Utility {
 		// clear editor panel
 		_new(panel);
 
-		// open file dialog
-		int result = fileChooser.showOpenDialog(parent);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			jsonFilePath = fileChooser.getSelectedFile().getAbsolutePath();
-		}
-
 		try {
+			// open file dialog
+			int result = fileChooser.showOpenDialog(parent);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				jsonFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+			}
+			if(!jsonFilePath.matches("[a-zA-Z1-9/]+\\.json$"))
+				throw new IOException("only .json file allowed!");
+			
+			//read file
 			BufferedReader br;
 			br = new BufferedReader(new FileReader(jsonFilePath));
 			String jsonData = br.readLine();
@@ -90,115 +94,126 @@ public class Utility {
 
 			panel.repaint();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(parent, e.getMessage());
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(parent, e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e);
+			JOptionPane.showMessageDialog(parent, e.getMessage());
 		}
 	}
 
 	public static void save(Window parent, EditorPanel panel){
-		// if file path not chosen
-		if (jsonFilePath == null) {
-			int result = fileChooser.showSaveDialog(parent);
-			if (result == JFileChooser.APPROVE_OPTION) {
-				jsonFilePath = fileChooser.getSelectedFile().getAbsolutePath();
-			}
-		}
-
-		// make json data
-		Component[] components = panel.getComponents();
-
-		StringBuilder jsonData = new StringBuilder("");
-		//opening bracket 추가
-		jsonData.append("{\"components\":[");
-		//components to json
-		for (Component component : components) {
-			MockComponent mock = (MockComponent)component;
-			jsonData.append(mock.toJson()+",");
-		}
-		// 마지막 , 지운다
-		int lastIndex = jsonData.length()-1;
-		jsonData.deleteCharAt(lastIndex);
-		//closing bracket 추가
-		jsonData.append("]}");
-
-		// write json data to file
 		try {
+			// if file path not chosen
+			if (jsonFilePath == null) {
+				int result = fileChooser.showSaveDialog(parent);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					jsonFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+				}
+			}
+			if(!jsonFilePath.matches("[a-zA-Z1-9/]+\\.json$"))
+				throw new IOException("only .json file allowed!");
+			
+			// make json data
+			Component[] components = panel.getComponents();
+	
+			StringBuilder jsonData = new StringBuilder("");
+			//opening bracket 추가
+			jsonData.append("{\"components\":[");
+			//components to json
+			for (Component component : components) {
+				MockComponent mock = (MockComponent)component;
+				jsonData.append(mock.toJson()+",");
+			}
+			// 마지막 , 지운다
+			int lastIndex = jsonData.length()-1;
+			jsonData.deleteCharAt(lastIndex);
+			//closing bracket 추가
+			jsonData.append("]}");
+	
+			// write json data to file
 			FileWriter fw = new FileWriter(jsonFilePath);
 			fw.write(jsonData.toString());
 			fw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			jsonFilePath = null;
+			JOptionPane.showMessageDialog(parent, e.getMessage());
 		}
 	}
 
 	public static void saveAs(Window parent, EditorPanel panel) {
-		// open file dialog
-		int result = fileChooser.showSaveDialog(parent);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			jsonFilePath = fileChooser.getSelectedFile().getAbsolutePath();
-		}
-
-		// make json data
-		Component[] components = panel.getComponents();
-
-		StringBuilder jsonData = new StringBuilder("");
-
-		jsonData.append("{\"components\":[");
-		for (Component component : components) {
-			MockComponent mock = (MockComponent)component;
-			jsonData.append(mock.toJson());
-		}
-		jsonData.append("]}");
-
-		// write json data to file
 		try {
+			// if file path not chosen
+			int result = fileChooser.showSaveDialog(parent);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				jsonFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+			}
+			if(!jsonFilePath.matches("[a-zA-Z1-9/]+\\.json$"))
+				throw new IOException("only .json file allowed!");
+			
+			// make json data
+			Component[] components = panel.getComponents();
+	
+			StringBuilder jsonData = new StringBuilder("");
+			//opening bracket 추가
+			jsonData.append("{\"components\":[");
+			//components to json
+			for (Component component : components) {
+				MockComponent mock = (MockComponent)component;
+				jsonData.append(mock.toJson()+",");
+			}
+			// 마지막 , 지운다
+			int lastIndex = jsonData.length()-1;
+			jsonData.deleteCharAt(lastIndex);
+			//closing bracket 추가
+			jsonData.append("]}");
+	
+			// write json data to file
 			FileWriter fw = new FileWriter(jsonFilePath);
 			fw.write(jsonData.toString());
 			fw.close();
 		} catch (IOException e) {
-			//TODO: show message dialog
+			JOptionPane.showMessageDialog(parent, e.getMessage());
 		}
 	}
 	public static void genCode(Window parent, EditorPanel panel){
-		// open file dialog
-		int result = fileChooser.showSaveDialog(parent);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			javaFilePath = fileChooser.getSelectedFile().getAbsolutePath();
-		}
-		
-		String firstHalf = "import java.awt.BorderLayout;\nimport java.awt.EventQueue;\n\nimport javax.swing.JFrame;\nimport javax.swing.JPanel;\nimport javax.swing.border.EmptyBorder;\n\npublic class test extends JFrame {\n\n\tprivate JPanel contentPane;\n\n\t/**\n\t * Launch the application.\n\t */\n\tpublic static void main(String[] args) {\n\t\tEventQueue.invokeLater(new Runnable() {\n\t\t\tpublic void run() {\n\t\t\t\ttry {\n\t\t\t\t\ttest frame = new test();\n\t\t\t\t\tframe.setVisible(true);\n\t\t\t\t} catch (Exception e) {\n\t\t\t\t\te.printStackTrace();\n\t\t\t\t}\n\t\t\t}\n\t\t});\n\t}\n\n\t/**\n\t * Create the frame.\n\t */\n\tpublic test() {\n\t\tsetDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);\n\t\tsetBounds(100, 100, 450, 300);\n\t\tcontentPane = new JPanel();\n\t\tcontentPane.setBorder(new EmptyBorder(5, 5, 5, 5));\n\t\tcontentPane.setLayout(new BorderLayout(0, 0));\n\t\tsetContentPane(contentPane);\n";
-		String lastHalf = "\t}\n\n}\n";
-		
-		//generate java code
-		StringBuilder code = new StringBuilder("");
-		code.append(firstHalf);
-		
-		Component[] components = panel.getComponents();
-		
-		//TODO: toJavaCode imcomplete
-		for(Component component:components){
-			MockComponent mock = (MockComponent)component;
-			code.append(mock.toCode());
-		}
-		code.append(lastHalf);
-		
-		//write code to file
-		FileWriter fw;
-		try {
+		try {	
+			// open file dialog
+			int result = fileChooser.showSaveDialog(parent);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				javaFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+			}
+			if(!jsonFilePath.matches("[a-zA-Z1-9/]+\\.java$"))
+				throw new IOException("only .java file allowed!");
+			
+			String firstHalf = "import java.awt.BorderLayout;\nimport java.awt.EventQueue;\n\nimport javax.swing.JFrame;\nimport javax.swing.JPanel;\nimport javax.swing.border.EmptyBorder;\n\npublic class test extends JFrame {\n\n\tprivate JPanel contentPane;\n\n\t/**\n\t * Launch the application.\n\t */\n\tpublic static void main(String[] args) {\n\t\tEventQueue.invokeLater(new Runnable() {\n\t\t\tpublic void run() {\n\t\t\t\ttry {\n\t\t\t\t\ttest frame = new test();\n\t\t\t\t\tframe.setVisible(true);\n\t\t\t\t} catch (Exception e) {\n\t\t\t\t\te.printStackTrace();\n\t\t\t\t}\n\t\t\t}\n\t\t});\n\t}\n\n\t/**\n\t * Create the frame.\n\t */\n\tpublic test() {\n\t\tsetDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);\n\t\tsetBounds(100, 100, 450, 300);\n\t\tcontentPane = new JPanel();\n\t\tcontentPane.setBorder(new EmptyBorder(5, 5, 5, 5));\n\t\tcontentPane.setLayout(new BorderLayout(0, 0));\n\t\tsetContentPane(contentPane);\n";
+			String lastHalf = "\t}\n\n}\n";
+			
+			//generate java code
+			StringBuilder code = new StringBuilder("");
+			code.append(firstHalf);
+			
+			Component[] components = panel.getComponents();
+			
+			//TODO: toJavaCode imcomplete
+			for(Component component:components){
+				MockComponent mock = (MockComponent)component;
+				code.append(mock.toCode());
+			}
+			code.append(lastHalf);
+			
+			//write code to file
+			FileWriter fw;
+			
 			String rootPath = System.getProperty("user.dir");
 			
 			fw = new FileWriter(javaFilePath);
 			fw.write(code.toString());
 			fw.close();
 		} catch (IOException e) {
-			//TODO: show message dialog
+			jsonFilePath = null;
+			JOptionPane.showMessageDialog(parent, e.getMessage());
 		}
 	}
 	public static void close() {
